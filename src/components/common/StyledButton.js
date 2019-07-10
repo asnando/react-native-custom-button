c09e17cfb2f74512955779af5350b648887bf5f6
Fernando Rama <fernandorama.dev@gmail.com>
2019-07-10 12:22:48 -0300

    Align text with flex instead of line-height

--- src/components/common/StyledButton.js
+++ src/components/common/StyledButton.js
@@ -14,6 +14,8 @@ const StyledButton = styled.TouchableOpacity`
   border-color: ${buttonDefaultBorderColor};
   background-color: ${props => props.buttonTintColor || buttonDefaultColor};
   width: ${buttonDefaultWidth};
+  align-items: center;
+  justify-content: center;
 `;
 
 export default StyledButton;
