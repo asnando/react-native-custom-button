f862f22c7bbde31008ed1e3d5019842ae56f4694
Fernando Rama <fernandorama.dev@gmail.com>
2019-07-11 17:37:02 -0300

    Update default font size

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,14 @@
 import PropTypes from 'prop-types';
 
 export const buttonHeight = 48;
-export const buttonFontSize = 20;
+export const buttonFontSize = 18;
 export const buttonBorderRadius = 4;
 export const buttonVerticalMargin = 8;
 export const buttonDefaultColor = '#2196F3';
 export const buttonDefaultBorderColor = '#2196F3';
 export const buttonDefaultFontColor = 'white';
 export const buttonDefaultWidth = 256;
-export const buttonIconSize = 48;
+export const buttonIconSize = 42;
 export const buttonIconBorderRadius = 4;
 
 export const buttonPropTypes = {
